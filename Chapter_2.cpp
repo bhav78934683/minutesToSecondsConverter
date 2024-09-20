de01335824c65dcf79c0c84e4c1d778141c6b716
Bhavya Devarsu <81049749+bhav78934683@users.noreply.github.com>
2024-09-20 19:04:46 -0400

    Minutes to seconds converter
    This program converts minutes to seconds and seconds to minutes.

---
+++ Chapter_2.cpp
@@ -0,0 +1,283 @@
+
+#include <iostream>
+using namespace std;
+
+int main ()
+
+{
+	double num;
+	int number ;
+
+	cout << "A double is stored in "
+  		  << sizeof(double) << " bytes.\n";
+	cout << "Variable number is stored in "
+  		  << sizeof(number)  << " bytes.\n";
+
+
+return 0;
+
+}
+/*
+int main()
+{
+	float num, num1, num2;
+
+	num1 = 4/2;
+	num2 = 5/2;
+	num = 5%2;
+
+	cout << num1 <<"  " <<num2 <<"  "<<num <<endl;
+
+	return 0;
+}*/
+
+// This program converts seconds to minutes and seconds.
+/*
+int main()
+{
+	// variable for total seconds
+	int totalSeconds;
+   // Variables for minutes and seconds
+	int minutes, seconds;
+
+	cout << "Enter the total seconds: " ;  //prompt
+	cin >> totalSeconds;
+
+	// Get the number of minutes.
+	minutes = totalSeconds / 60;
+
+	// Get the remaining seconds.
+	seconds = totalSeconds % 60;
+
+	// Display the results.
+	cout << endl<<totalSeconds << " is equivalent to:\n";
+	cout << "Minutes: " << minutes << endl;
+	cout << "Seconds: " << seconds << endl;
+	return 0;
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
